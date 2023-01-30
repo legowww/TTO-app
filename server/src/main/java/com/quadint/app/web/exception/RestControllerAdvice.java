13deae5bafb2316e684ef2b243d2ed9dc906d7ba
PHJ <legowww@gmail.com>
2023-01-31 04:03:05 +0900

    EmptyResultDataAccessException 예외 처리 핸들러 추가
    단일 데이터를 예상했지만 데이터가 존재하지 않는 경우 발생하는 예외

--- server/src/main/java/com/quadint/app/web/exception/RestControllerAdvice.java
+++ server/src/main/java/com/quadint/app/web/exception/RestControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.quadint.app.web.exception;
 
 import com.quadint.app.web.controller.response.Response;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +19,13 @@ public class RestControllerAdvice {
         return ResponseEntity.ok().body(Response.error(e.getMessage()));
     }
 
+    /* jdbc 에서 하나의 데이터를 예상했지만 데이터가 존재하지 않는 exception handle */
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity ttoHandler(EmptyResultDataAccessException e) {
+        return ResponseEntity.ok().body(Response.error("unfounded id error"));
+    }
+
+    /* @Validation exception handle */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         String errorMessage = e.getBindingResult()
