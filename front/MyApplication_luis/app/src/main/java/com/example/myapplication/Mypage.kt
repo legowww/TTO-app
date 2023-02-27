ce24aadc98acd4d121e7ed4dc27f5f3757e69af4
Musix <ljw927@naver.com>
2023-02-27 11:37:06 +0900

    version 1.2.4 - Design modified

--- front/MyApplication_luis/app/src/main/java/com/example/myapplication/Mypage.kt
+++ front/MyApplication_luis/app/src/main/java/com/example/myapplication/Mypage.kt
@@ -1,9 +1,17 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
+import android.graphics.drawable.shapes.RectShape
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.dto.Favorite
@@ -23,6 +31,24 @@ class Mypage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+        val imageV : (ImageView) = findViewById(R.id.imageV)
+        val bitmap: Bitmap = Bitmap.createBitmap(1100, 1000, Bitmap.Config.ARGB_8888)
+        val canvas: Canvas = Canvas(bitmap)
+
+        var shapeDrawable: ShapeDrawable
+
+        shapeDrawable = ShapeDrawable(RectShape())
+        shapeDrawable.setBounds( 0, 365, 1100, 1000)
+        shapeDrawable.getPaint().setColor(Color.parseColor("#71a300"))
+        shapeDrawable.draw(canvas)
+
+        shapeDrawable = ShapeDrawable(OvalShape())
+        shapeDrawable.setBounds( 0, 220, 1100, 500)
+        shapeDrawable.getPaint().setColor(Color.parseColor("#71a300"))
+        shapeDrawable.draw(canvas)
+
+        imageV.background = BitmapDrawable(getResources(), bitmap)
 
         //val back = Intent(this,MainActivity::class.java)
         val star = Intent(this,Favorites::class.java)
